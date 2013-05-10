940cecf76fcb89fcd7005ad6299fcacef55eba12
Douglas Duteil <douglasduteil@gmail.com>
2013-05-10 14:14:55 +0200

    [M] Add a home made requireCss

--- core/main.js
+++ core/main.js
@@ -1,6 +1,26 @@
 (function () {
   var _ = "assets/vendor/";
 
+  /* =Require css
+   -----------------------------------------------------------------------------*/
+  window.requireCss = (function () {
+    var registry = {}, loadCss;
+
+    loadCss = function (url) {
+      var link = document.createElement("link");
+      link.type = "text/css";
+      link.rel = "stylesheet";
+      link.href = url;
+      document.getElementsByTagName("head")[0].appendChild(link);
+    };
+
+    return function (url) {
+      if (registry[url]) return;
+      registry[url] = true;
+      loadCss(url);
+    };
+  })();
+
   /* =Launcher
    -----------------------------------------------------------------------------*/
   requirejs(
