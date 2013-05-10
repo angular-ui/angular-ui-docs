7a0e6f93d126d11660d4f4dc2e80ad52d56d4ac6
Douglas Duteil <douglasduteil@gmail.com>
2013-05-10 11:47:37 +0200

    [M] Separate ssh/config

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,27 +1,31 @@
 # Inspired by http://sleepycoders.blogspot.se/2013/03/sharing-travis-ci-generated-files.html
 
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
-
-  echo -e "\n>>> Current Repo:$REPO --- Travis Branch:$TRAVIS_BRANCH"
+  echo -e "\n>>> Current Repo:$REPO --- Travis Branch:$TRAVIS_BRANCH\n"
 
 
-  echo -e "\n>>> CGrunt the doc."
+  echo -e ">>> Grunt the doc.\n"
   grunt build-doc
-  cp -R doc $HOME/doc
+  cp -R out $HOME/out
 
+
+  echo -e ">>> Clone the gh-pages branch.\n"
   cd $HOME
-  echo -e "\n>>> Cloning repo build branch."
   git clone --branch=gh-pages $REPO  tmp-gh-pages
 
+
+  echo -e ">>> Replace files.\n"
   cd tmp-gh-pages
-  cp -Rf $HOME/doc/* .
+  cp -Rf $HOME/out/* .
 
 
-  echo -e "\n>>> Force adding and commiting."
+  echo -e ">>> Force add and commit.\n"
   git add -f .
   git commit -m "Travis commit : build $TRAVIS_BUILD_NUMBER"
-  echo -e "\n>>> Pushing result :"
+
+
+  echo -e ">>> Push result :"
   git push -fq origin gh-pages
 
   echo -e "All done.\n"
--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -7,7 +7,16 @@ git remote set-url origin $REPO.git
 git config --global user.email "angular-ui@googlegroups.com"
 git config --global user.name "AngularUI (via TravisCI)"
 
+if [ -z "$id_rsa_{1..23}" ]; then echo 'No $id_rsa_{1..23} found !' ; exit 1; fi
+
 echo -n $id_rsa_{1..23} >> ~/.ssh/travis_rsa_64
-base64 --decode --ignore-garbage ~/.ssh/travis_rsa_64 > ~/.ssh/travis_rsa
-chmod 600 ~/.ssh/travis_rsa
-echo -e "Host github.com\n\tUser\tgit\n\tIdentityFile\t~/.ssh/travis_rsa\n\tStrictHostKeyChecking\tno\n\tCheckHostIP\tno\n" >> ~/.ssh/config
+base64 --decode --ignore-garbage ~/.ssh/travis_rsa_64 > ~/.ssh/id_rsa
+
+chmod 600 ~/.ssh/id_rsa
+
+echo -e ">>> Copy config"
+mv -fv out/.travis/ssh-config ~/.ssh/config
+
+echo -e ">>> Hi github.com !"
+ssh -T git@github.com
+echo -e "\n"
